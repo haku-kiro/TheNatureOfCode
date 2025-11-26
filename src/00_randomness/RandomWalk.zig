326369df7cdd6488d821888a62a1be4b9b27336f
Michael Da Costa <mdjcosta30@gmail.com>
2025-11-26 20:39:37 +0000

    drawing on a canvas instead of just the screen, to get rid of flashing

--- src/00_randomness/RandomWalk.zig
+++ src/00_randomness/RandomWalk.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 const rl = @import("raylib");
 
-// Random number setup;
 var prng = std.Random.DefaultPrng.init(42);
 const random = prng.random();
 
@@ -9,8 +8,6 @@ const Walker = struct {
     x: c_int,
     y: c_int,
     fn step(self: *Walker) void {
-        // Walking in either of 8 possible directions,
-        // or stay in the same position;
         const xdir = random.intRangeAtMost(i8, -1, 1);
         const ydir = random.intRangeAtMost(i8, -1, 1);
 
@@ -27,20 +24,27 @@ pub fn main() !void {
 
     rl.setTargetFPS(30);
 
-    // Setting the background color once (to have a "trail" effect);
-    rl.clearBackground(rl.Color.white);
+    const canvas = try rl.loadRenderTexture(screenWidth, screenHeight);
+    defer rl.unloadRenderTexture(canvas);
 
-    // setup,
+    // Clear the canvas once at the start
+    rl.beginTextureMode(canvas);
+    rl.clearBackground(rl.Color.white);
+    rl.endTextureMode();
+
     var walker = Walker{ .x = screenWidth / 2, .y = screenHeight / 2 };
 
     while (!rl.windowShouldClose()) {
-        rl.beginDrawing();
-        defer rl.endDrawing();
-
-        // Prevent flashing by having this... But you don't have "trail" effect
-        rl.clearBackground(rl.Color.white);
-
+        // Draw to the persistent canvas
+        rl.beginTextureMode(canvas);
         rl.drawPixel(walker.x, walker.y, rl.Color.black);
+        rl.endTextureMode();
+
         walker.step();
+
+        // Draw the canvas to the screen
+        rl.beginDrawing();
+        rl.drawTexture(canvas.texture, 0, 0, rl.Color.white);
+        rl.endDrawing();
     }
 }
